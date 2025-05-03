52519c0537fc610f7d71da6a07d507d15fd146f6
Sakr <sakrismail265@gmail.com>
2025-05-03 23:07:47 +0200

    updated set _seed

--- python/faster_rcnn/faster_rcnn_MobileNet_augmented.py
+++ python/faster_rcnn/faster_rcnn_MobileNet_augmented.py
@@ -26,9 +26,6 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
 
 set_seed(42)
 
