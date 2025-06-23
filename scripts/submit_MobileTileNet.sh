0e62a33bd6a44dc5b33a6f09a761c7d38b7d4c13
SakrIsmail <sakrismail265@gmail.com>
2025-06-23 19:32:59 +0200

    script for MobileTileNet

---
+++ scripts/submit_MobileTileNet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=MobileTileNet
+#SBATCH --output=outputs/yolo/output_MobileTileNet
+#SBATCH --error=outputs/yolo/error_MobileTileNet
+#SBATCH --time=24:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+
+
+module load cuda12.3/toolkit/12.3
+module load cuDNN/cuda12.3/9.1.0.70
+
+eval "$(conda shell.bash hook)"
+
+# Activate your conda environment
+conda activate /var/scratch/sismail/my_env
+
+# Run your Python script
+python python/MobileTileNet.py
+
+conda deactivate
